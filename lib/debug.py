429bf9b31c87405ca68037b9e860cfcd8680777e
Natalie Wanjiru <natalie.wanjiru@student.moringaschool.com>
2023-08-29 08:30:40 +0300

    New commit

--- lib/debug.py
+++ lib/debug.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 
-from config import CONN, CURSOR
-from song import Song
+from lib import CONN, CURSOR
+from lib.song import Song
+
+def reset_database():
+    Song.drop_table()
+    Song.create_table()
+    Song.create("Hello", "25")
+    Song.create("99 Problems", "The Black Album")
 
 
-if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+reset_database()
 
+import pytest; pytest.set_trace()
--- lib/song.py
+++ lib/song.py
@@ -7,9 +7,8 @@ class Song:
         self.name = name
         self.album = album
 
-
     @classmethod
-    def create_table(self):
+    def create_table(cls):
         sql = """
             CREATE TABLE IF NOT EXISTS songs (
                 id INTEGER PRIMARY KEY,
@@ -17,28 +16,28 @@ class Song:
                 album TEXT
             )
         """
-
         CURSOR.execute(sql)
-
+        CONN.commit()
 
     def save(self):
         sql = """
             INSERT INTO songs (name, album)
             VALUES (?, ?)
         """
-
         CURSOR.execute(sql, (self.name, self.album))
+        self.id = CURSOR.lastrowid  # Assign the last inserted row ID to the instance
+        CONN.commit()
 
-   
     @classmethod
-
     def create(cls, name, album):
         song = Song(name, album)
         song.save()
         return song
-    
 
+# Create the songs table if it doesn't exist
+Song.create_table()
+
+# Create a new song and print its name and album
 song = Song.create("Hello", "25")
 print(song.name)
 print(song.album)
-
